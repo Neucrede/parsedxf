6e17918ef339edb3fa7ec445a61efa87312fbce5
neucrede <liqinpeng2014@sina.com>
2021-03-20 15:53:09 +0800

    Platform indept. memory mapped IO

---
+++ src/memmap.c
@@ -0,0 +1,120 @@
+#include "memmap.h"
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+
+memmap_fd_t memmap_open(const char *filename, int flag, mode_t mode)
+{
+    HANDLE h;
+    DWORD access = GENERIC_READ;
+    int acc_mode = flag & O_ACCMODE;
+    DWORD dispos = OPEN_ALWAYS;
+
+    access |= (acc_mode & O_RDONLY) ? GENERIC_READ : 0;
+    access |= (acc_mode & O_WRONLY || acc_mode & O_RDWR) ? GENERIC_WRITE : 0;
+
+    if (flag & O_CREAT) {
+        if (flag & O_EXCL) {
+            dispos = CREATE_NEW;
+        }
+        else {
+            dispos = OPEN_ALWAYS;
+        }
+    } 
+    else if (flag & O_TRUNC) {
+        dispos = TRUNCATE_EXISTING;
+    }
+
+    h = CreateFile(filename, access, FILE_SHARE_READ, NULL, dispos, 
+            FILE_ATTRIBUTE_NORMAL, NULL);
+
+    (void)mode;
+
+    return (memmap_fd_t)h;
+}
+
+int memmap_close(memmap_fd_t fd)
+{
+    return CloseHandle((HANDLE)fd) != 0 ? 0 : -1;
+}
+
+void *memmap_map(void *address, size_t length, int protect, int flags, memmap_fd_t fd, off_t offset)
+{
+    HANDLE mapobj;
+    void *map_addr;
+    DWORD flprotect = PAGE_READONLY;
+    DWORD access = FILE_MAP_READ;
+
+    if (flags & MEMMAP_PRIVATE) {
+        if (protect & MEMMAP_READWRITE) {
+            flprotect = PAGE_WRITECOPY;
+            access = FILE_MAP_COPY;
+        }
+    } 
+    else if (flags & MEMMAP_SHARED) {
+        if (protect & MEMMAP_READWRITE) {
+            flprotect = PAGE_READWRITE;
+            access = FILE_MAP_ALL_ACCESS;
+        }
+    }
+
+    mapobj = CreateFileMapping((HANDLE)fd, NULL, flprotect, 0, 0, NULL);
+    if (mapobj == NULL) {
+        return NULL;
+    }
+
+    map_addr = MapViewOfFileEx(
+            mapobj, 
+            access, 
+            (DWORD)(offset >> 32), 
+            (DWORD)(offset & 0xFFFFFFFF), 
+            (DWORD)length, 
+            address);
+
+    return map_addr;
+}
+
+int memmap_unmap(void *addr, size_t length)
+{
+    (void)length;
+
+    return UnmapViewOfFile(addr) != 0 ? 0 : -1;
+}
+
+int memmap_sync(void *addr, size_t length, int flags)
+{
+    (void)length;
+    (void)flags;
+
+    return FlushViewOfFile(addr, 0) != 0 ? 0 : -1;
+}
+
+
+#elif defined(__linux__) || defined(__unix__) || defined(_POSIX_VERSION)
+
+memmap_fd_t memmap_open(const char *filename, int flag, mode_t mode)
+{
+    return open(filename, flag, mode);
+}
+
+int memmap_close(memmap_fd_t fd)
+{
+    return close(fd);
+}
+
+void *memmap_map(void *address, size_t length, int protect, int flags, memmap_fd_t fd, off_t offset)
+{
+    return mmap(address, length, protect, flags, fd, offset);
+}
+
+int memmap_unmap(void *addr, size_t length)
+{
+    return munmap(addr, length);
+}
+
+int memmap_sync(void *addr, size_t length, int flags)
+{
+    return msync(addr, length, flags);
+}
+
+#endif
+
---
+++ src/memmap.h
@@ -0,0 +1,58 @@
+#ifndef __MEMMAP_H__
+#define __MEMMAP_H__
+
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+
+#include <windows.h>
+
+/* protect */
+#define MEMMAP_READ 1
+#define MEMMAP_WRITE 2
+#define MEMMAP_READWRITE MEMMAP_WRITE
+
+/* mmap flags */
+#define MEMMAP_PRIVATE 1
+#define MEMMAP_SHARED 2
+
+/* sync flags */
+#define MEMMAP_SYNC 1
+#define MEMMAP_ASYNC 2
+
+typedef HANDLE memmap_fd_t;
+
+#elif defined(__linux__) || defined(__unix__) || defined(_POSIX_VERSION)
+
+#include <sys/mman.h>
+#include <unistd.h>
+
+/* protect */
+#define MEMMAP_READ PROT_READ
+#define MEMMAP_WRITE PROT_WRITE
+#define MEMMAP_READWRITE MEMMAP_WRITE
+
+/* mmap flags */
+#define MEMMAP_PRIVATE MAP_PRIVATE
+#define MEMMAP_SHARED MAP_SHARED
+
+/* sync flags */
+#define MEMMAP_SYNC MS_SYNC
+#define MEMMAP_ASYNC MS_ASYNC
+
+typedef int memmap_fd_t;
+
+#else
+#error "Unknown platform."
+#endif
+
+memmap_fd_t memmap_open(const char *filename, int flag, mode_t mode);
+int memmap_close(memmap_fd_t fd);
+void *memmap_map(void *address, size_t length, int protect, int flags, memmap_fd_t fd, off_t offset);
+int memmap_unmap(void *addr, size_t length);
+int memmap_sync(void *addr, size_t length, int flags);
+
+#endif /* __MEMMAP_H__ */
+
